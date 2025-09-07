3ab1ed06cd00f06e9714a4ab3635953c9cb4e25e
kj-hs <daleyantics@gmail.com>
2025-09-07 08:54:56 -0300

    Added absidian-nvim plugin

---
+++ lua/custom/plugins/obsidain-nvim.lua
@@ -0,0 +1,89 @@
+return {
+  'obsidian-nvim/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = 'lms',
+        path = '~/git/lms',
+      },
+      {
+        name = 'wms',
+        path = '~/git/wms',
+      },
+    },
+    daily_notes = {
+      folder = '50_Daily',
+      date_format = '%Y-%m-%d',
+      default_tags = { 'daily-notes' },
+    },
+    completion = {
+      nvim_cmp = true,
+    },
+    new_notes_location = '00_Inbox',
+    templates = {
+      folder = '30_Resources/Templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
+    },
+    -- Optional, configure additional syntax highlighting / extmarks.
+    -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+    ui = {
+      enable = true, -- set to false to disable all additional syntax features
+      ignore_conceal_warn = false, -- set to true to disable conceallevel specific warning
+      update_debounce = 200, -- update delay after a text change (in milliseconds)
+      max_file_length = 5000, -- disable UI features for files with more than this many lines
+      -- Define how various check-boxes are displayed
+      checkboxes = {
+        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+        [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+        ['x'] = { char = '', hl_group = 'ObsidianDone' },
+        ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+        ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+        ['!'] = { char = '', hl_group = 'ObsidianImportant' },
+        -- Replace the above with this if you don't have a patched font:
+        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+        -- You can also add more custom ones...
+      },
+      -- Use bullet marks for non-checkbox lists.
+      bullets = { char = '•', hl_group = 'ObsidianBullet' },
+      external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      -- Replace the above with this if you don't have a patched font:
+      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+      reference_text = { hl_group = 'ObsidianRefText' },
+      highlight_text = { hl_group = 'ObsidianHighlightText' },
+      tags = { hl_group = 'ObsidianTag' },
+      block_ids = { hl_group = 'ObsidianBlockID' },
+      hl_groups = {
+        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+        ObsidianTodo = { bold = true, fg = '#f78c6c' },
+        ObsidianDone = { bold = true, fg = '#89ddff' },
+        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+        ObsidianTilde = { bold = true, fg = '#ff5370' },
+        ObsidianImportant = { bold = true, fg = '#d73128' },
+        ObsidianBullet = { bold = true, fg = '#89ddff' },
+        ObsidianRefText = { underline = true, fg = '#c792ea' },
+        ObsidianExtLinkIcon = { fg = '#c792ea' },
+        ObsidianTag = { italic = true, fg = '#89ddff' },
+        ObsidianBlockID = { italic = true, fg = '#89ddff' },
+        ObsidianHighlightText = { bg = '#75662e' },
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
